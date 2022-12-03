c272cb1a156e90c9feb167752135e1390decaa00
yurch1knaprekole <102054190+yurch1knaprekole@users.noreply.github.com>
2022-12-03 13:59:04 +0500

    Add files via upload

---
+++ src/components/form/form.jsx
@@ -0,0 +1,48 @@
+// Render Prop
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import './form.scss';
+
+const Basic = () => (
+  <div className='container'>
+        <div className='login'><h3>Login</h3></div>
+        <div className='line-first'></div>
+        <div className='line-second'></div>
+    
+    <Formik
+      
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default Basic;
